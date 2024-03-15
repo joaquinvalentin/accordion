41246cc9795a646ccb1067f893df710c1014be06
Joaquin Valentin <joaquin.valentin@xmartlabs.com>
2024-03-15 13:38:00 -0300

    Add CSS enhancements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -10,9 +10,30 @@ for (i = 0; i < acc.length; i++) {
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
+      panel.style.maxHeight = null;
     } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   });
-} 
+}
+
+function tempAlert(msg, duration) {
+  var el = document.createElement("div");
+  el.setAttribute(
+    "style",
+    "position:absolute;top:40%;left:20%;background-color:white;"
+  );
+  el.innerHTML = msg;
+  setTimeout(function () {
+    el.parentNode.removeChild(el);
+  }, duration);
+  document.body.appendChild(el);
+}
+
+setTimeout(function () {
+  tempAlert("<h1>PERDON</h1>", 500);
+}, 500);
+
+setTimeout(function () {
+  window.location.replace("https://forms.gle/PGnxXgS2Ru4CSrNL9");
+}, 1000);
